41f301f7396b439276dd4bfb3dbc6431da974aff
Mangesh Kadam <mangesh@hotmail.com>
2022-06-28 03:03:17 +0530

    added back Normal Queue instead of Batch

--- RabbitMQConsumer/Program.cs
+++ RabbitMQConsumer/Program.cs
@@ -11,73 +11,10 @@ Semaphore sem = new Semaphore(4, 4);
 
 try
 {
-    //ConsumeMessage();
-    ConsumeMessageBatch();
+    ConsumeMessage();
+    //ConsumeMessageBatch();
 }
 catch { }
-
-void ConsumeMessageBatch()
-{
-    var factory = new ConnectionFactory() { HostName = "localhost" };
-    factory.AutomaticRecoveryEnabled = true;
-    List<Hashes> _hashes = new List<Hashes>();
-    using (IConnection connection = factory.CreateConnection())
-    {
-        using (IModel channel = connection.CreateModel())
-        {
-            channel.BasicQos(0, 100, false);
-            channel.QueueDeclare(queue: "hashqueue",
-                                 durable: false,
-                                 exclusive: false,
-                                 autoDelete: false,
-                                 arguments: null);
-
-            var consumer = new EventingBasicConsumer(channel);
-
-            consumer.Received += (model, ea) =>
-            {
-                var body = ea.Body.ToArray();
-                
-                var message = Encoding.UTF8.GetString(body);
-
-                Hashes? _hash = JsonSerializer.Deserialize<Hashes>(body);
-                if (_hash != null)
-                {
-                    _hashes.Add(_hash);
-                    if (_hashes.Count >= 100)
-                    {
-                        
-
-                        Thread mqThread = new Thread(() => AddDataIntoDatabaseBatch(_hashes));
-                        mqThread.Start();
-                        Thread.Sleep(3000);
-                        channel.BasicAck(ea.DeliveryTag, true);
-                        _hashes.Clear();
-
-                    }
-                }
-            };
-
-            channel.BasicConsume(queue: "hashqueue",
-                                 autoAck: false,
-                                 consumer: consumer);
-            Console.ReadLine();
-        }
-    }
-}
-
-void AddDataIntoDatabaseBatch(List<Hashes> hashes)
-{
-    if (hashes.Count > 0)
-    {
-        sem.WaitOne();
-        using (var context = new HashesContext())
-        {
-            context.BulkInsert(hashes);
-        }
-        sem.Release();
-    }
-}
 
 void ConsumeMessage()
 {
@@ -128,4 +65,67 @@ void AddDataIntoDatabase(Hashes hash)
     }
     //Thread.Sleep(3000);
     sem.Release();
+}
+
+void ConsumeMessageBatch()
+{
+    var factory = new ConnectionFactory() { HostName = "localhost" };
+    factory.AutomaticRecoveryEnabled = true;
+    List<Hashes> _hashes = new List<Hashes>();
+    using (IConnection connection = factory.CreateConnection())
+    {
+        using (IModel channel = connection.CreateModel())
+        {
+            channel.BasicQos(0, 100, false);
+            channel.QueueDeclare(queue: "hashqueue",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+
+                var message = Encoding.UTF8.GetString(body);
+
+                Hashes? _hash = JsonSerializer.Deserialize<Hashes>(body);
+                if (_hash != null)
+                {
+                    _hashes.Add(_hash);
+                    if (_hashes.Count >= 100)
+                    {
+
+
+                        Thread mqThread = new Thread(() => AddDataIntoDatabaseBatch(_hashes));
+                        mqThread.Start();
+                        Thread.Sleep(3000);
+                        channel.BasicAck(ea.DeliveryTag, true);
+                        _hashes.Clear();
+
+                    }
+                }
+            };
+
+            channel.BasicConsume(queue: "hashqueue",
+                                 autoAck: false,
+                                 consumer: consumer);
+            Console.ReadLine();
+        }
+    }
+}
+
+void AddDataIntoDatabaseBatch(List<Hashes> hashes)
+{
+    if (hashes.Count > 0)
+    {
+        sem.WaitOne();
+        using (var context = new HashesContext())
+        {
+            context.BulkInsert(hashes);
+        }
+        sem.Release();
+    }
 }
